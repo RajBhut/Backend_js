9b65d9ad7bdeda08a25c84b7042d96028df3bab9
Raj_Bhut <rajbhut2832005@gmail.com>
2024-04-17 11:32:13 +0530

    add  new github rout

--- api/index.js
+++ api/index.js
@@ -1,10 +1,37 @@
-const express = require('express')
+
 require('dotenv').config()
-const app = express()
-const port = process.env.PORT
 
-app.get('/', (req , res) => {res.send("Hellow world!!")})
+const axios = require('axios');
 
-app.listen(port , ()=>{
-    console.log(`app is listening on ${port}` )
-})
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/user/:id', (req, res) => {
+    res.send(`<h1> user id is ${req.params.id}</h1>`);
+});
+
+app.get('/username/:username', (req, res) => {
+    const { username } = req.params;
+    axios.get(`https://api.github.com/users/${username}`)
+        .then(response => {
+            const lin = response.data.avatar_url;
+
+            res.send(`<img src="${lin}" />`);
+          
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Error fetching user data');
+        });
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello world!!");
+});
+
+app.listen(port, () => {
+    console.log(`app is listening on ${port}`);
+});
+
